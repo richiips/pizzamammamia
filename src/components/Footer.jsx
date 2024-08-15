8dba86786522eac1ba2ddaefbe00b1e4bd426f6d
Richips <Richips@Richis-MacBook-Pro.local>
2024-08-15 15:46:12 -0400

    Probando cambio de user

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className='footerContainer'>
-        <p> © 2021 - Pizzería Mamma Mia! - Todos los derechos reservados.</p>
+        <p> © 2024 - Pizzería Mamma Mia! - Todos los derechos reservados.</p>
       </div>
     </>
   )
